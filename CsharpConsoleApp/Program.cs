1ee6dd4936f60702de25fe34c6a0716463d9707e
sout.rahim <sout.rahim@gmail.com>
2022-12-27 10:43:21 +0700

    Datat type

--- CsharpConsoleApp/Program.cs
+++ CsharpConsoleApp/Program.cs
@@ -5,7 +5,18 @@ namespace CsharpConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            string message = "Hello World";
+            int intVar = 10;
+            float floatVar = 10.2f;
+            char charVar = 'E';
+            bool boolVar = false;
+
+            Console.WriteLine(message);
+            Console.WriteLine(intVar);
+            Console.WriteLine(floatVar);
+            Console.WriteLine(charVar);
+            Console.WriteLine(boolVar);
+            Console.ReadKey();
         }
     }
 }

ec734b68ef25f469783065db97e0f43416e08172
sout.rahim <sout.rahim@gmail.com>
2022-12-27 10:36:43 +0700

    Add project files.

---
+++ CsharpConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+using System; // System is name space
+namespace CsharpConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
